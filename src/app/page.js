26b9031c776a039b08e8b9dab2d4d65d9b1e3d78
rodrigo745 <roodrigoquinteros@gmail.com>
2024-08-12 22:10:07 -0300

    btn switch

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,18 @@ import estilo from "./estilo.json";
 export default function Home() {
 
   const [ estiloGeneral, setEstiloGeneral ] = useState(estilo.light);
-  const { primario, secundario, fondo, texto, hoja } = estiloGeneral;
+  const { id, primario, secundario, fondo, texto, hoja } = estiloGeneral;
 
-  console.log(`bg-[#${primario}]`)
+  // Switch style
+  const cambiarEstilo = (e)=>{
+    e.preventDefault();
+    id === 0 ? setEstiloGeneral(estilo.dark) : setEstiloGeneral(estilo.light); 
+  }
 
   return (
     <div className="px-5 flex justify-center pt-10 pb-10"
       style={{background: fondo, color: texto}}>
-      <MenuGeneral secundario={secundario}/>
+      <MenuGeneral secundario={secundario} cambiarEstilo={cambiarEstilo}/>
       <div className="w-[60%]">
         <MenuTexto primario={primario}/>
         <div className="h-screen mt-10 rounded-md shadow-md p-2" style={{
